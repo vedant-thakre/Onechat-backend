a79162537066b62dd401f4ee7c946220e7f24b2f
Bhumik <bhumikdehane@gmail.com>
2023-07-16 06:55:33 -0700

    added login and signup functionality in backend

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,24 @@ const express = require("express");
 const chats = require("./data/data");
 const path = require('path');
 const connectDB = require("./config/db");
-
+const userRoutes = require("./Routes/userRoutes");
+const { notFound, errorHandler } = require("./middleWare/errorMiddleWare");
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 
 connectDB();
 const app = express();
 
+app.use(express.json());  // to accept json data
+
 app.get("/", (req, res) => {
     res.send("api is running");;
 });
 
-app.get("/api/chat", (req, res) => {
-    res.send(chats);
-}); 
+app.use('/api/user', userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-    const singleChat = chats.find(c => c._id === req.params.id);
-    res.send(singleChat);
-});
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
