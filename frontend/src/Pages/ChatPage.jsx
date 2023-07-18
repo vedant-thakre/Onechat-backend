990f893528919e566c5740affe29bf71d680feda
Bhumik <bhumikdehane@gmail.com>
2023-07-18 10:53:03 -0700

    built chat page UI

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,22 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
+import Chatbox from "../components/Chatbox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
 
-const ChatPage = () => {
-          const [chats, setchats] = useState([]);
 
-  const fetchChats = async () => {
-    const { data } = await axios.get("/api/chat");
-        setchats(data);
-    };
+const Chatpage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
 
-    useEffect(() => {
-        fetchChats();
-    }, []);
   return (
-    <div>
-      {chats.map(chat => (<div key={chat._id}>{ chat.chatName}</div>))}
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default Chatpage;
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, Container, Text } from '@chakra-ui/react';
 import { TabList, Tabs, Tab, TabPanels, TabPanel } from '@chakra-ui/react';
 import Login from '../components/Authentication/Login';
 import SignUp from '../components/Authentication/SignUp';
+import { useHistory } from 'react-router-dom';
 
 const HomePage = () => {
+
+  const history = useHistory();
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+    if (user) {
+      history.push(("/chats"));
+    }
+  }, [history]);
   return (
     <Container maxW="xl" centerContent>
       <Box
