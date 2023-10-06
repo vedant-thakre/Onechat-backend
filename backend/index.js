a3df8666c7c48eaf6c956bb2f881fe3f488c8ebe
Bhumik <bhumikdehane@gmail.com>
2023-10-06 19:24:16 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    console.log(path.resolve(__dirname1, "frontend", "build", "index.html"))
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   }
   );
