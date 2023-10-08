d9c286a2a92a299c285435016e891da085cb6495
Bhumik <bhumikdehane@gmail.com>
2023-10-08 18:50:03 +0530

    deployment changes

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
+const cors = require("cors");
+
 
 dotenv.config();
 connectDB();
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
 
 app.use(express.json()); // to accept json data
 
